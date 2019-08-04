c5d98b286e6074a796124c8b796207db5621d464
Simon Wiles <simonjwiles@gmail.com>
2019-08-04 13:44:07 -0700

    move page shadow effect to mixin

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,24 +27,7 @@ const Container = styled.div`
   padding: 10px;
   position: relative;
 
-  &::before,
-  &::after {
-    border-radius: 100px / 10px;
-    bottom: 10px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
-    content: "";
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 10px;
-    z-index: -1;
-  }
-
-  &::after {
-    left: auto;
-    right: 10px;
-    transform: skew(8deg) rotate(3deg);
-  }
+  ${props => props.theme.pageShadow}
 
   main {
     flex-grow: 1;
--- src/theme/apils-theme.js
+++ src/theme/apils-theme.js
@@ -1,5 +1,28 @@
 export default {
   // main: "hsl(230, 55%, 58%)"
   main: "#508084",
-  pageBg: "white"
+  pageBg: "white",
+
+  pageShadow: `
+    position: relative;
+
+    &::before,
+    &::after {
+      border-radius: 100px / 10px;
+      bottom: 10px;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
+      content: "";
+      left: 0;
+      position: absolute;
+      right: 0;
+      top: 10px;
+      z-index: -1;
+    }
+
+    &::after {
+      left: auto;
+      right: 10px;
+      transform: skew(8deg) rotate(3deg);
+    }
+  `
 };
