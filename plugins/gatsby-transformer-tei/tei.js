d300831e54f891b5bb6a8ea33cd3319574f8f453
Simon Wiles <simonjwiles@gmail.com>
2019-08-11 08:08:42 -0700

    Initial work on SAX parser for content

--- plugins/gatsby-transformer-tei/tei.js
+++ plugins/gatsby-transformer-tei/tei.js
@@ -6,20 +6,61 @@ function parseTei(xmlString) {
   return libxmljs.parseXml(xmlString);
 }
 
-function getText(teiDoc, xpath) {
+function getTextByXpath(teiDoc, xpathExpr) {
   // dealing with namespaces with libxml is a pain, and having
   // to remember to call .toString() is annoying to, so this
   // is a convenience function
   return teiDoc
-    .get(`${xpath.replace(/\/(?!\/)/g, "/tei:")}/text()`, ns)
+    .get(`${xpathExpr.replace(/\/(?!\/)/g, "/tei:")}/text()`, ns)
     .toString();
+}
+
+function getElemByXpath(teiDoc, xpathExpr) {
+  // dealing with namespaces with libxml is a pain, and having
+  // to remember to call .toString() is annoying to, so this
+  // is a convenience function
+  return teiDoc.get(`${xpathExpr.replace(/\/(?!\/)/g, "/tei:")}`, ns);
 }
 
 function getFrontmatter(teiDoc) {
   return {
-    title: getText(teiDoc, "//titleStmt/title"),
-    author: getText(teiDoc, "//titleStmt/author")
+    title: getTextByXpath(teiDoc, "//titleStmt/title"),
+    author: getTextByXpath(teiDoc, "//titleStmt/author")
   };
 }
 
-module.exports = { parseTei, getFrontmatter };
+function toHtml(xmlString) {
+  const parser = new libxmljs.SaxParser();
+
+  let openTag = null;
+  let currentString = "";
+  // parser.on('startDocument', ...);
+  parser.on("startElementNS", (tag, attrs, prefix, uri, namespace) => {
+    // console.log(tag, attrs);
+    openTag = tag;
+  });
+
+  parser.on("characters", chars => {
+    // console.log(`"${chars}"`);
+    currentString += chars;
+  });
+
+  parser.on("endElementNS", (tag, prefix, uri) => {
+    if (tag == openTag) {
+      console.log(`${tag}: ${currentString}`);
+      currentString = "";
+    }
+  });
+
+  parser.parseString(xmlString);
+}
+
+function getTextAsHtml(teiDoc) {
+  const front = getElemByXpath(teiDoc, "//text/front");
+  // const body = getElemByXpath(teiDoc, "//text/body");
+
+  toHtml(front.toString());
+  return "";
+}
+
+module.exports = { parseTei, getFrontmatter, getTextAsHtml };
