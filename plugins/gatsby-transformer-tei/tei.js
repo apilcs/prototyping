b055f96bff4568102f78ef507f5ebe4b3ce4fcd8
Simon Wiles <simonjwiles@gmail.com>
2019-08-11 08:01:26 -0700

    Initial work on `libxml` version

--- plugins/gatsby-transformer-tei/tei.js
+++ plugins/gatsby-transformer-tei/tei.js
@@ -1,11 +1,24 @@
+const libxmljs = require("libxmljs");
+
+const ns = { tei: "http://www.tei-c.org/ns/1.0" };
+
 function parseTei(xmlString) {
-  return {};
+  return libxmljs.parseXml(xmlString);
+}
+
+function getText(teiDoc, xpath) {
+  // dealing with namespaces with libxml is a pain, and having
+  // to remember to call .toString() is annoying to, so this
+  // is a convenience function
+  return teiDoc
+    .get(`${xpath.replace(/\/(?!\/)/g, "/tei:")}/text()`, ns)
+    .toString();
 }
 
 function getFrontmatter(teiDoc) {
   return {
-    title: "<title goes here>",
-    author: "<author goes here>"
+    title: getText(teiDoc, "//titleStmt/title"),
+    author: getText(teiDoc, "//titleStmt/author")
   };
 }
 
