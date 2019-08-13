a3d1f0c2e860efc3ab5813a6a35edda329a3fc94
Simon Wiles <simonjwiles@gmail.com>
2019-08-12 22:12:51 -0700

    Add automatic markup of Chinese

--- plugins/gatsby-transformer-tei/tei.js
+++ plugins/gatsby-transformer-tei/tei.js
@@ -32,7 +32,31 @@ class TeiDoc extends JsonMlDoc {
   }
 
   getAbstractHtml() {
-    return this.toHtml(this.getElemByPath("div[@type='abstract']"));
+    return TeiDoc.markupChinese(
+      this.toHtml(this.getElemByPath("div[@type='abstract']"))
+    );
+  }
+
+  static markupChinese(text) {
+    const chineseUnicodeRanges = [
+      "[\u4e00-\u9fff]", // CJK Unified Ideographs
+      "[\u3400-\u4dbf]", // CJK Unified Ideographs Extension A
+      "[\u{20000}-\u{2a6df}]", // CJK Unified Ideographs Extension B
+      "[\u{2a700}-\u{2b73f}]", // CJK Unified Ideographs Extension C
+      "[\u{2b740}-\u{2b81f}]", // CJK Unified Ideographs Extension D
+      "[\u{2b820}-\u{2ceaf}]", // CJK Unified Ideographs Extension E
+      "[\u{2ceb0}-\u{2ebef}]", // CJK Unified Ideographs Extension F
+      "[\uf900-\ufaff]", // CJK Compatibility Ideographs
+      "[\u3300-\u33ff]", // CJK Compatibility
+      "[\ufe30-\ufe4f]", // CJK Compatibility Forms
+      "[\u{2f800}-\u{2fa1f}]" // CJK Compatibility Ideographs Supplement
+    ];
+
+    const anyChineseRegex = new RegExp(
+      `(?<chinese>(?:${chineseUnicodeRanges.join("|")})+)`,
+      "ug"
+    );
+    return text.replace(anyChineseRegex, "<span class='zh'>$<chinese></span>");
   }
 }
 
