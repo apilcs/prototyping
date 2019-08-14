28ba69da84de927dadee2f8e2c8fc51d35072ecd
Simon Wiles <simonjwiles@gmail.com>
2019-08-13 22:32:47 -0700

    Render article body in article page

--- src/pages/article.js
+++ src/pages/article.js
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import { injectIntl } from "gatsby-plugin-intl"; // Link, FormattedMessage
-import Parser from "html-react-parser";
+import Parser, { domToReact } from "html-react-parser";
 // import { intlShape } from "react-intl";
 import styled from "styled-components";
 
 import Layout from "../components/layout";
 import Meta from "../components/meta";
+
+import SectionDivider from "../ui-components/section-divider";
 
 export const query = graphql`
   query {
@@ -18,6 +20,7 @@ export const query = graphql`
       }
       titleHtml
       abstractHtml
+      articleBodyHtml
       footnotesHtml
     }
   }
@@ -31,6 +34,16 @@ const Abstract = styled.blockquote`
   em,
   span.zh {
     font-style: normal;
+  }
+`;
+
+const ArticleBody = styled.div`
+  font-size: 0.9rem;
+
+  section > header:first-child {
+    font-family: ${props => props.theme.fonts.header};
+    font-size: 1.4rem;
+    margin: 1rem 0;
   }
 `;
 
@@ -51,6 +64,19 @@ const StaticPage = ({ data }) => {
       <h1>{Parser(tei.titleHtml)}</h1>
       <h3>{author}</h3>
       <Abstract>{Parser(tei.abstractHtml)}</Abstract>
+      <ArticleBody>
+        {Parser(tei.articleBodyHtml, {
+          replace: domNode => {
+            if (domNode.name === "section")
+              return (
+                <>
+                  <section>{domToReact(domNode.children)}</section>
+                  <SectionDivider />
+                </>
+              );
+          }
+        })}
+      </ArticleBody>
       <Footnotes>{Parser(tei.footnotesHtml)}</Footnotes>
     </Layout>
   );
