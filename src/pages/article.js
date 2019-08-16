1c34faad5678ccf2c3591ef30b3c06e71f2cbd41
Simon Wiles <simonjwiles@gmail.com>
2019-08-15 19:36:44 -0700

    Add images to `article.js`
    This isn't really working exactly as it should, but it will do for now.

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
+// import { Img } from "gatsby-image";
 import { injectIntl } from "gatsby-plugin-intl"; // Link, FormattedMessage
 import Parser, { domToReact } from "html-react-parser";
 // import { intlShape } from "react-intl";
@@ -24,9 +25,24 @@ export const query = graphql`
       abstractHtml
       articleBodyHtml
       footnotesHtml
+      images {
+        publicURL
+        relativePath
+      }
     }
   }
 `;
+
+// childImageSharp {
+//   fluid(maxWidth: 400, maxHeight: 250) {
+//     ...GatsbyImageSharpFluid
+//   }
+// }
+
+// Can't get this to work with the gatsby-image `<Img>` component
+//  (hence the commenting out here and below)
+// Same problem here:
+//  https://stackoverflow.com/questions/57381690/gatsby-image-mystery
 
 const Abstract = styled.blockquote`
   font-size: 0.9rem;
@@ -61,9 +77,51 @@ const Footnotes = styled.div`
   }
 `;
 
+// domNode = { type: 'tag',
+//   name: 'br',
+//   attribs: {},
+//   children: [],
+//   next: null,
+//   prev: null,
+//   parent: null }
+
 const StaticPage = ({ data }) => {
   const { tei } = data;
   const { title, author } = tei.frontmatter;
+
+  const parserOptions = {
+    replace: domNode => {
+      if (domNode.name === "section")
+        return (
+          <>
+            <section>{domToReact2(domNode.children)}</section>
+            <SectionDivider color={ApilsTheme.colors.main} />
+          </>
+        );
+
+      if (domNode.name === "img")
+        return (
+          <img
+            src={
+              tei.images.find(img =>
+                img.relativePath.endsWith(domNode.attribs.src)
+              ).publicURL
+            }
+            alt=""
+          />
+          // <Img
+          //   fluid={
+          //     tei.images.find(img =>
+          //       img.relativePath.endsWith(domNode.attribs.src)
+          //     ).childImageSharp.fluid
+          //   }
+          //   alt=""
+          // />
+        );
+    }
+  };
+
+  const domToReact2 = children => domToReact(children, parserOptions);
 
   return (
     <Layout>
@@ -71,19 +129,7 @@ const StaticPage = ({ data }) => {
       <h1>{Parser(tei.titleHtml)}</h1>
       <h3>{author}</h3>
       <Abstract>{Parser(tei.abstractHtml)}</Abstract>
-      <ArticleBody>
-        {Parser(tei.articleBodyHtml, {
-          replace: domNode => {
-            if (domNode.name === "section")
-              return (
-                <>
-                  <section>{domToReact(domNode.children)}</section>
-                  <SectionDivider color={ApilsTheme.colors.main} />
-                </>
-              );
-          }
-        })}
-      </ArticleBody>
+      <ArticleBody>{Parser(tei.articleBodyHtml, parserOptions)}</ArticleBody>
       <Footnotes>{Parser(tei.footnotesHtml)}</Footnotes>
     </Layout>
   );
