9e3c21dcc618be38ab7cb9ed2795d7736957875b
Simon Wiles <simonjwiles@gmail.com>
2019-09-25 22:35:11 -0700

    Add NER tools controls

--- src/pages/proofing.js
+++ src/pages/proofing.js
@@ -1,39 +1,50 @@
 import React from "react";
-import { css } from "styled-components";
 
 import { post } from "axios";
 
 import Layout from "../components/layout";
 import Meta from "../components/meta";
 
+import { ArticleBody, Footnotes } from "../ui-components/article-elements";
 import {
-  Abstract,
-  ArticleBody,
-  Footnotes
-} from "../ui-components/article-elements";
+  ControlsPanel,
+  controlsTipStyles,
+  ControlButton
+} from "../ui-components/proofing-elements";
 import Tooltip from "../ui-components/tooltip";
 import LoadingSpinner from "../ui-components/loading-spinner";
 
 import TeiDoc from "../../plugins/gatsby-transformer-tei/tei";
 import TeiRenderer from "../utils/tei-renderer";
 
-const controlsTipStyles = css`
-  ${({ theme }) => theme.scale(-0.3)};
-  background: black;
-  color: white;
-  font-family: ${({ theme }) => theme.fonts.header};
-  font-style: normal;
-  max-width: 20rem;
-  padding: 0.5rem 1rem;
-  z-index: 2;
-`;
+const ENTITY_TYPES = [
+  ["PERSON", "People, including fictional."],
+  ["NORP", "Nationalities or religious or political groups."],
+  ["FAC", "Buildings, airports, highways, bridges, etc."],
+  ["ORG", "Companies, agencies, institutions, etc."],
+  ["GPE", "Countries, cities, states."],
+  ["LOC", "Non-GPE locations, mountain ranges, bodies of water."],
+  ["PRODUCT", "Objects, vehicles, foods, etc. (Not services.)"],
+  ["EVENT", "Named hurricanes, battles, wars, sports events, etc."],
+  ["WORK_OF_ART", "Titles of books, songs, etc."],
+  ["LAW", "Named documents made into laws."],
+  ["LANGUAGE", "Any named language."],
+  ["DATE", "Absolute or relative dates or periods."],
+  ["TIME", "Times smaller than a day."],
+  ["PERCENT", "Percentage, including ”%“."],
+  ["MONEY", "Monetary values, including unit."],
+  ["QUANTITY", "Measurements, as of weight or distance."],
+  ["ORDINAL", "“first”, “second”, etc."],
+  ["CARDINAL", "Numerals that do not fall under another type."]
+];
 
 class ProofingPage extends React.Component {
-  static fileUpload(file) {
+  static sendFileForNerMarkup(file, model, include) {
     const url = "http://localhost:5000/";
     const formData = new FormData();
     formData.append("xml", file);
-    formData.set("model", "en_core_web_sm");
+    formData.set("model", model);
+    formData.set("include", include);
     const config = {
       headers: { "content-type": "multipart/form-data" }
     };
@@ -55,20 +66,27 @@ class ProofingPage extends React.Component {
     this.setState({ teiFile, loading: true }, () => this.readTeiFile());
   };
 
-  onNerClick = (/* event */) => {
+  onNerSubmit = event => {
+    event.preventDefault();
+    const include = Array.from(event.target.include)
+      .filter(elem => elem.checked)
+      .map(elem => elem.value)
+      .join("|");
     this.setState({ loading: true });
-    this.getMarkedUpTei();
+    this.getMarkedUpTei(event.target.model.value, include);
   };
 
   onRenderClick = (/* event */) => this.readTeiFile();
 
   onClearClick = (/* event */) => this.setState({ teiNode: null });
 
-  getMarkedUpTei() {
+  getMarkedUpTei(model, include) {
     const { teiFile } = this.state;
-    ProofingPage.fileUpload(teiFile).then(response => {
-      this.setState({ teiXml: response.data }, () => this.makeTeiNode());
-    });
+    ProofingPage.sendFileForNerMarkup(teiFile, model, include).then(
+      response => {
+        this.setState({ teiXml: response.data }, () => this.makeTeiNode());
+      }
+    );
   }
 
   makeTeiNode() {
@@ -97,13 +115,12 @@ class ProofingPage extends React.Component {
   }
 
   render() {
-    const { teiNode, loading } = this.state;
+    const { teiFile, teiNode, loading } = this.state;
     const article = teiNode
       ? (() => {
           const teiRenderer = new TeiRenderer(teiNode);
           return (
             <>
-              <Abstract>{teiRenderer.abstract()}</Abstract>
               <ArticleBody>{teiRenderer.articleBody()}</ArticleBody>
               <Footnotes>{teiRenderer.footnotes()}</Footnotes>
             </>
@@ -118,26 +135,109 @@ class ProofingPage extends React.Component {
         ) : (
           ""
         )}
-        <h1>Proofing Page</h1>
-        <div>
-          <input type="file" name="input-file" onChange={this.onNewFile} />
-          <Tooltip
-            tipContent="Send document for NER markup and render result."
-            tipStyles={controlsTipStyles}>
-            <button type="button" onClick={this.onNerClick}>
-              NER
-            </button>
-          </Tooltip>
+        <ControlsPanel>
+          <div>
+            <input type="file" name="input-file" onChange={this.onNewFile} />
+            {teiFile && !teiNode ? (
+              <ControlButton type="button" onClick={this.onRenderClick}>
+                Re-Render
+              </ControlButton>
+            ) : (
+              ""
+            )}
+            {teiNode ? (
+              <ControlButton type="button" onClick={this.onClearClick}>
+                Clear
+              </ControlButton>
+            ) : (
+              ""
+            )}
+          </div>
           {teiNode ? (
-            <button type="button" onClick={this.onClearClick}>
-              Clear
-            </button>
+            <>
+              <h4 onClick={event => event.target.classList.toggle("shown")}>
+                NER Tools
+              </h4>
+              <form onSubmit={this.onNerSubmit}>
+                <div>
+                  <fieldset>
+                    <legend>Model</legend>
+                    <label htmlFor="en_core_web_sm">
+                      <input
+                        type="radio"
+                        name="model"
+                        id="en_core_web_sm"
+                        value="en_core_web_sm"
+                        defaultChecked
+                      />
+                      en_core_web_sm
+                    </label>
+                    <label htmlFor="en_core_web_lg">
+                      <input
+                        type="radio"
+                        name="model"
+                        id="en_core_web_lg"
+                        value="en_core_web_lg"
+                      />
+                      en_core_web_lg
+                    </label>
+                  </fieldset>
+                  <fieldset>
+                    <legend>Include</legend>
+                    {ENTITY_TYPES.map(([entityType, entityDesc]) => (
+                      <div key={entityType}>
+                        <Tooltip
+                          placement="right"
+                          tipContent={entityDesc}
+                          tipStyles={controlsTipStyles}>
+                          <label htmlFor={entityType}>
+                            <input
+                              type="checkbox"
+                              name="include"
+                              id={entityType}
+                              value={entityType}
+                              defaultChecked
+                            />
+                            {entityType}
+                          </label>
+                        </Tooltip>
+                      </div>
+                    ))}
+                    <div>
+                      <ControlButton
+                        type="button"
+                        onClick={event =>
+                          Array.from(event.target.form.include).forEach(
+                            checkbox => (checkbox.checked = true)
+                          )
+                        }>
+                        Select All
+                      </ControlButton>
+                      <ControlButton
+                        type="button"
+                        onClick={event =>
+                          Array.from(event.target.form.include).forEach(
+                            checkbox => (checkbox.checked = false)
+                          )
+                        }>
+                        Select None
+                      </ControlButton>
+                    </div>
+                  </fieldset>
+                </div>
+                <div>
+                  <Tooltip
+                    tipContent="Send document for NER markup and render result."
+                    tipStyles={controlsTipStyles}>
+                    <ControlButton type="submit">Markup Entities</ControlButton>
+                  </Tooltip>
+                </div>
+              </form>
+            </>
           ) : (
-            <button type="button" onClick={this.onRenderClick}>
-              Re-Render
-            </button>
+            ""
           )}
-        </div>
+        </ControlsPanel>
         {article}
       </Layout>
     );
--- src/ui-components/proofing-elements.js
+++ src/ui-components/proofing-elements.js
@@ -1,10 +1,113 @@
 import styled, { css } from "styled-components";
+import { lighten } from "polished";
 import { Tip } from "./tooltip";
+
+const ControlsPanel = styled.div`
+  background: #eaeaea;
+  margin: 10px;
+  padding: 10px;
+  ${({ theme }) => theme.scale(-0.3)};
+  ${"" /* font-family: ${({ theme }) => theme.fonts.header}; */}
+  font-family: "Source Sans Pro", sans-serif;
+  overflow: hidden;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  h4 {
+    margin: 1em 0 0 1em;
+
+    &::before {
+      color: ${({ theme }) => theme.colors.main};
+      content: "⧐";
+      display: inline-block;
+      margin-right: 4px;
+      transition: transform 0.5s ease;
+    }
+
+    &.shown {
+      &::before {
+        transform: rotate(90deg);
+      }
+
+      & + form {
+        padding: 1em 0;
+        height: auto;
+      }
+    }
+  }
+
+  form {
+    transition: padding 1s ease;
+    overflow: hidden;
+    height: 0;
+    padding: 0;
+    margin-bottom: 0;
+
+    div {
+      align-items: start;
+    }
+  }
+
+  fieldset {
+    display: flex;
+    flex: 1 0 auto;
+    padding: 0 2em 1em;
+    margin: 0 1em 1em;
+
+    div:last-child {
+      margin-top: 1em;
+    }
+  }
+
+  input[type="checkbox"],
+  input[type="radio"] {
+    margin-right: 5px;
+  }
+
+  input[type="file"] {
+    flex-grow: 1;
+    line-height: 1.2;
+  }
+
+  label {
+    cursor: pointer;
+    padding-right: 10px;
+  }
+`;
+
+const controlsTipStyles = css`
+  ${({ theme }) => theme.scale(-0.3)};
+  background: black;
+  color: white;
+  font-style: normal;
+  max-width: 20rem;
+  padding: 0 8px;
+  z-index: 2;
+`;
+
+const ControlButton = styled.button`
+  background: ${({ theme }) => theme.colors.main};
+  color: white;
+
+  border: 0;
+  border-radius: 3px;
+  margin: 0 4px;
+
+  line-height: 1.2;
+  cursor: pointer;
+
+  &:hover {
+    background: ${({ theme }) => lighten(0.1, theme.colors.main)};
+  }
+`;
 
 const entityTipStyles = css`
   ${({ theme }) => theme.scale(-0.5)};
   background: #eaeaea;
-  border: 1px solid black;
+  border: 1px solid darkgrey;
   color: black;
   font-family: sans-serif;
   font-style: normal;
@@ -51,4 +154,11 @@ const NamedEntity = styled.span`
   }
 `;
 
-export { NamedEntity, entityTipStyles };
+export {
+  ControlsPanel,
+  controlsTipStyles,
+  ControlButton,
+  NamedEntity,
+  entityTipStyles
+  // FileInput
+};
